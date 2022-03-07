563aad03445f2a7cb8fdd47e2129f699d7c4c6fd
Bahaa <Bahaa.Elramlawi300@gmail.com>
2022-03-07 23:12:43 +0200

    Initial commit

---
+++ lib/screens/note_list.dart
@@ -0,0 +1,235 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
+import 'package:notes_app/db_helper/db_helper.dart';
+import 'package:notes_app/modal_class/notes.dart';
+import 'package:notes_app/screens/note_detail.dart';
+import 'package:notes_app/screens/search_note.dart';
+import 'package:notes_app/utils/widgets.dart';
+import 'package:sqflite/sqflite.dart';
+
+class NoteList extends StatefulWidget {
+  const NoteList({Key key}) : super(key: key);
+
+  @override
+  State<StatefulWidget> createState() {
+    return NoteListState();
+  }
+}
+
+class NoteListState extends State<NoteList> {
+  DatabaseHelper databaseHelper = DatabaseHelper();
+  List<Note> noteList;
+  int count = 0;
+  int axisCount = 2;
+
+  @override
+  Widget build(BuildContext context) {
+    if (noteList == null) {
+      noteList = [];
+      updateListView();
+    }
+
+    Widget myAppBar() {
+      return AppBar(
+        title: const Text('Notes'),
+        centerTitle: true,
+        elevation: 0,
+        backgroundColor: Colors.black,
+        leading: noteList.isEmpty
+            ? Container()
+            : IconButton(
+                splashRadius: 22,
+                icon: const Icon(
+                  Icons.search,
+                  color: Colors.white,
+                ),
+                onPressed: () async {
+                  final Note result = await showSearch(
+                      context: context, delegate: NotesSearch(notes: noteList));
+                  if (result != null) {
+                    navigateToDetail(result, 'Edit Note');
+                  }
+                },
+              ),
+        actions: <Widget>[
+          noteList.isEmpty
+              ? Container()
+              : IconButton(
+                  splashRadius: 22,
+                  icon: Icon(
+                    axisCount == 2 ? Icons.list : Icons.grid_on,
+                    color: Colors.white,
+                  ),
+                  onPressed: () {
+                    setState(() {
+                      axisCount = axisCount == 2 ? 4 : 2;
+                    });
+                  },
+                )
+        ],
+      );
+    }
+
+    return Scaffold(
+      appBar: myAppBar(),
+      body: noteList.isEmpty
+          ? Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const Padding(
+                    padding: EdgeInsets.all(16.0),
+                    child: Icon(
+                      Icons.info,
+                      color: Colors.black45,
+                      size: 200,
+                    ),
+                  ),
+                  Text('Add a new note!',
+                      style: Theme.of(context).textTheme.bodyText2),
+                ],
+              ),
+            )
+          : Container(
+              color: Colors.white,
+              child: getNotesList(),
+            ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          navigateToDetail(Note('', '', 3, 0), 'Add Note');
+        },
+        tooltip: 'Add Note',
+        shape: const CircleBorder(
+            side: BorderSide(color: Colors.white, width: 2.0)),
+        child: const Icon(Icons.add, color: Colors.white),
+        backgroundColor: Colors.black,
+      ),
+    );
+  }
+
+  Widget getNotesList() {
+    return StaggeredGridView.countBuilder(
+      physics: const BouncingScrollPhysics(),
+      crossAxisCount: 4,
+      itemCount: count,
+      itemBuilder: (BuildContext context, int index) => GestureDetector(
+        onTap: () {
+          navigateToDetail(noteList[index], 'Edit Note');
+        },
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Container(
+            padding: const EdgeInsets.all(8.0),
+            decoration: BoxDecoration(
+                color: colors[noteList[index].color],
+                border: Border.all(width: 2, color: Colors.black),
+                borderRadius: BorderRadius.circular(8.0)),
+            child: Column(
+              children: <Widget>[
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    Expanded(
+                      child: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Text(
+                          noteList[index].title,
+                          style: Theme.of(context).textTheme.bodyText2,
+                        ),
+                      ),
+                    ),
+                    Text(
+                      getPriorityText(noteList[index].priority),
+                      style: TextStyle(
+                          color: getPriorityColor(noteList[index].priority)),
+                    ),
+                  ],
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: <Widget>[
+                      Expanded(
+                        child: Text(noteList[index].description ?? '',
+                            style: Theme.of(context).textTheme.bodyText1),
+                      )
+                    ],
+                  ),
+                ),
+                Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: <Widget>[
+                      Text(noteList[index].date,
+                          style: Theme.of(context).textTheme.subtitle2),
+                    ])
+              ],
+            ),
+          ),
+        ),
+      ),
+      staggeredTileBuilder: (int index) => StaggeredTile.fit(axisCount),
+      mainAxisSpacing: 4.0,
+      crossAxisSpacing: 4.0,
+    );
+  }
+
+  // Returns the priority color
+  Color getPriorityColor(int priority) {
+    switch (priority) {
+      case 1:
+        return Colors.red;
+        break;
+      case 2:
+        return Colors.yellow;
+        break;
+      case 3:
+        return Colors.green;
+        break;
+
+      default:
+        return Colors.yellow;
+    }
+  }
+
+  // Returns the priority icon
+  String getPriorityText(int priority) {
+    switch (priority) {
+      case 1:
+        return '!!!';
+        break;
+      case 2:
+        return '!!';
+        break;
+      case 3:
+        return '!';
+        break;
+
+      default:
+        return '!';
+    }
+  }
+
+  void navigateToDetail(Note note, String title) async {
+    bool result = await Navigator.push(context,
+        MaterialPageRoute(builder: (context) => NoteDetail(note, title)));
+
+    if (result == true) {
+      updateListView();
+    }
+  }
+
+  void updateListView() {
+    final Future<Database> dbFuture = databaseHelper.initializeDatabase();
+    dbFuture.then((database) {
+      Future<List<Note>> noteListFuture = databaseHelper.getNoteList();
+      noteListFuture.then((noteList) {
+        setState(() {
+          this.noteList = noteList;
+          count = noteList.length;
+        });
+      });
+    });
+  }
+}
